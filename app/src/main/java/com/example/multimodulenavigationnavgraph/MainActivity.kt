40901f23c710c96b73534bb5101685e314ae49b7
seregatheone <sergey20030707@gmail.com>
2022-06-19 20:57:13 +0300

    scaffold

---
+++ app/src/main/java/com/example/multimodulenavigationnavgraph/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.multimodulenavigationnavgraph
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.multimodulenavigationnavgraph.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
