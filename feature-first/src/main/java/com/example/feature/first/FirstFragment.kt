318f857fb395f3d73796eff516daec3e3fd15ef4
seregatheone <sergey20030707@gmail.com>
2022-07-06 18:55:44 +0300

    fragments created

--- feature-first/src/main/java/com/example/feature/first/FirstFragment.kt
+++ feature-first/src/main/java/com/example/feature/first/FirstFragment.kt
@@ -5,6 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.feature.first.databinding.FragmentFirstBinding
 
-class FirstFragment : Fragment(R.layout.fragment_first) {
+class FirstFragment : Fragment() {
+    private var _binding : FragmentFirstBinding? = null
+    private val binding get () = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFirstBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
 }
--- feature-second/src/main/java/com/example/feature/second/SecondFragment.kt
+++ feature-second/src/main/java/com/example/feature/second/SecondFragment.kt
@@ -5,7 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.feature.second.databinding.FragmentSecondBinding
 
-class SecondFragment : Fragment(R.layout.fragment_second) {
+class SecondFragment : Fragment() {
+    private var _binding : FragmentSecondBinding? = null
+    private val binding get () = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSecondBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
 
 }
